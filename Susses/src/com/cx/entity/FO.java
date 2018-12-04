020ff3c1451b6bdb1c46bcfc84f891dae68b9679
Maisty <874311554@qq.com>
2018-12-04 15:29:21 +0800

    com

--- Susses/src/com/cx/entity/FO.java
+++ Susses/src/com/cx/entity/FO.java
@@ -1,5 +1,25 @@
 package com.cx.entity;
 
 public class FO {
+	private Order order;
+	
+	private Food food;
 
+	public Order getOrder() {
+		return order;
+	}
+
+	public void setOrder(Order order) {
+		this.order = order;
+	}
+
+	public Food getFood() {
+		return food;
+	}
+
+	public void setFood(Food food) {
+		this.food = food;
+	}
+	
+	
 }
